d870ee8b367ec51327f9a0bbab7725cd598d9443
Aaron Rosado <arosad2@protonmail.ch>
2023-04-07 18:27:09 -0400

    Run on remote server.

--- example.py
+++ example.py
@@ -12,7 +12,7 @@ from datasets import load_dataset, Audio
 
 # Set environmental variables for computer the code will run in
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "7"
+os.environ['CUDA_VISIBLE_DEVICES'] = '7'
 # os.environ["HF_HOME"] = '/opt/localdata/Data/laryn/hugging_face/'
 #
 # print(os.environ["HF_HOME"])
